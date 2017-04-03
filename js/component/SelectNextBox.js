9e2df708031ac86d271a7fc3323a59d8c321da39
Phil Goo Kang <philgookang@gmail.com>
2017-04-03 20:06:28 +0900

    added element direct call for next select

--- js/component/SelectNextBox.js
+++ js/component/SelectNextBox.js
@@ -20,7 +20,7 @@ var SelectNextBox = (function() {
     };
     that.clickRemove = function( target_1, target_2 ) {
         var list_to_add = [];
-        target_1.children().each(function() {
+        $(target_1).children().each(function() {
             if ( $(this).is(":selected") ) {
                 list_to_add.push({
                     "text" : $(this).text(),
@@ -31,7 +31,7 @@ var SelectNextBox = (function() {
         });
         for (var i = 0; i < list_to_add.length; i++) {
             var item = list_to_add[i];
-            target_2.append(
+            $(target_2).append(
                 '<option value="'+item.val+'">'+item.text+'</option>'
             );
         }
